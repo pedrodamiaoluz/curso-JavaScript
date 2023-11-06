6a1fa6fa466b5e5f079377791294743ff65c72d3
Pedro <pedrodamiao26061998@gmail.com>
2023-11-06 17:12:20 +0000

    Atualizado

---
+++ Aula11/index.js
@@ -0,0 +1,9 @@
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel} Km/h`);
+
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. Multado`)
+}else{
+
+    console.log(`Dirija sempre com sinto de segurança`);
+}
