447298427a448713a9f37572bbfb88041b0a1a83
Pedro <pedrodamiao26061998@gmail.com>
2023-11-09 17:12:17 +0000

    Atualizado

--- Exercício/exe14/script.js
+++ Exercício/exe14/script.js
@@ -9,11 +9,14 @@ function carregar(){
     if(hora >= 0 && hora < 12){
         //Bom Dia!!
         img.src = `fotomanha.png`
-    }else if(hora >= 12 && hora < 18){
+        document.body.style.backgroundColor = '#e2cd9f'
+    }else if(hora >= 12 && hora <= 18){
         // Boa Tarde!!
         img.src = `fototarde.png`
+        document.body.style.background = '#b9846f'
     }else{
         //Boa Noite!!
         img.src = `fotonoite.png`
+        document.body.style.background = '#515154'
     }
 }
