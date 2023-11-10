bdcd59fb57b4aec976e711f59824084fcebacc32
Pedro <pedrodamiao26061998@gmail.com>
2023-11-10 17:08:53 +0000

    Atualizado

--- Exercício/exe15/script.js
+++ Exercício/exe15/script.js
@@ -1,22 +1,54 @@
 function verifica(){
    var data = new Date()
    var ano = data.getFullYear()
-   var fano = document.getElementById('texano')
+   var fano = document.querySelector('input#txtano')
    var res = document.querySelector('div#res')
-   var formu = Number(fano.value)
-   if(formu == 0 || formu > ano){
+   var formul = Number(fano.value)
+   if(formul == 0 || Number(formul.value) > ano){
      window.alert(`[ERRO] Verifique os dados e tente novamente`)
-   }else{
-     var fsex = document.getElementsByClassName('radsex')
+   } 
+   else {
+     var fsex = document.getElementsByName('radsex')
      var idade = ano - Number(fano.value)
      var genero = ''
-     if(fsex[0].checked) {
+     var img = document.querySelector('img#foto')
+     //var img = document.createElement('img')
+     //img.setAttribute('id', 'foto')
+     if (fsex[0].checked) {
         genero = 'Homem'
+        if(idade >= 0 && idade <= 10){
+         // criança masculina
+           img.setAttribute('src', './imagem/fotobebemas.png')
+        }else if(idade <= 21){
+         // jovem masculino
+           img.setAttribute('src', './imagem/fotojovmasc.png')
+        } else if(idade <= 50){
+         // Adulto masculino
+           img.setAttribute('src', './imagem/fotoadultohom.png')
+        }else{
+         // idoso masculino
+           img.setAttribute('src', './imagem/fotovelhohom.png')
+        }
      }
      else if(fsex[1].checked) {
         genero = 'Mulher'
+        if(idade >= 0 && idade <= 10){
+         // criança feminina
+          img.setAttribute('src', './imagem/fotobebefem.png')
+        }else if(idade <= 21){
+         // jovem feminina
+          img.setAttribute('src', './imagem/fotojovfem.png')
+        }else if(idade <= 50){
+         // Adulto feminina
+          img.setAttribute('src', './imagem/fotoadultomulh.png')
+        }else{
+         // idoso feminina
+          img.setAttribute('src', './imagem/fotovelhamul.png')
+        }
      }
-     res.innerHTML = `Detectando ${genero} com ${idade}`
+      // res.style.textAlign = 'center'
+      res.innerHTML = `Detectando ${genero} com ${idade} anos`
+      res.appendChild(img)
    }
    
 }
