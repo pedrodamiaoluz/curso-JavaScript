25976bb39565da0659ecb608a228b70e7fd877ca
Pedro <pedrodamiao26061998@gmail.com>
2023-11-13 17:05:43 +0000

    Atualizado

---
+++ Aula13/anbiente.js
@@ -0,0 +1,31 @@
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 2); // condição no final*/
+   
+/*// Estrutura de Repetição while:
+var c = 1
+ while (c <= 6) { // faz teste logico no inicio
+ console.log(`Passo ${c}`)
+ c++
+}*/
+ /*
+ console.log('Ola!! Tudo Bem')
+ console.log('Ola!! Tudo Bem')
+ console.log('Ola!! Tudo Bem')
+ console.log('Ola!! Tudo Bem')
+ console.log('Ola!! Tudo Bem')
+ */
+   
+   
+   
+   
+   
+
+
+
+
+
+
