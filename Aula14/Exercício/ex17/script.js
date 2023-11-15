0a95be30eacef76a1cfe4413a75c0d85cf2d89a2
Pedro <pedrodamiao26061998@gmail.com>
2023-11-15 11:13:41 +0000

    Atualizado

--- Aula14/Exercício/ex17/script.js
+++ Aula14/Exercício/ex17/script.js
@@ -1,11 +1,20 @@
 function tabuada(){
-    let td = document.getElementById('textd')
-    let res = document.getElementById('res')
-    let valor = Number(td.value)
-     
-    for(let c = 0; c <= 10; c++){
-      let t = valor * c
-      res.innerHTML += `<br> ${valor} * ${c} = ${t} `
-}   
-  
+    let num = document.getElementById('textd')
+    let tab = document.getElementById('seltab')
+    
+    if(num.value.length == 0){
+      alert('[Erro!] Por facor insira o número.')
+    }else{
+      let n = Number(num.value)
+      tab.innerHTML = ''
+      for(let c = 1; c <= 10; c++){
+        let v1 = document.createElement('option')
+        v1.innerHTML =  `${n} x ${c} = ${n*c}`
+        v1.value = `tab${c}`
+        tab.appendChild(v1)
+     }
+    
+   }    
 }
+         
+    
